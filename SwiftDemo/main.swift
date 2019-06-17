d50a9597302cc341db5e6fa65c5dc316e0f78df8
gang.zhou <gang.zhou@gangzhoudeMacBook-Air.local>
2019-06-17 16:44:05 +0800

    添加sqlite Demo

--- SwiftDemo/main.swift
+++ SwiftDemo/main.swift
@@ -7,7 +7,7 @@
 //
 
 import Foundation
-
+import SQLite3
 print("Hello, World!")
 
 let dispatchQueue = DispatchQueue.init(label: "MyDispathQueue"
@@ -25,6 +25,13 @@ func encodedPostParams(params:[String:String]) -> Data {
     eParams.removeLast()
     return eParams.data(using: .utf8)!
 }
+
+struct BaseResponse:Decodable {
+    var result:Int8?
+    var description:String?
+    var systemTime:Int64?
+}
+
 
 /**
     发送网络请求
@@ -47,7 +54,7 @@ func doPost(_ postUrl:String,params:[String:String]?){
                 print(Thread.current)
             } else {
                 if let data = data,0 < data.count {
-                    print(String(data: data, encoding: .utf8)!)
+                    print(try! JSONDecoder().decode(BaseResponse.self, from: data))
                     print(Thread.current)
                 } else {
                     print("返回为NULL")
@@ -82,10 +89,10 @@ if name.count > 0 {
     print(name)
 }
 
-for i in 1...1{
-    doPost("https://www.mytian.com.cn/myt_market/userAction_login.do"
-        , params: ["user.phone":"18307207411","user.password":"123456"
-            ,"deviceType":"android","app_channel":"mytian","client_version":"93"])
+for _ in 1...1 {
+doPost("https://www.mytian.com.cn/myt_market/userAction_login.do"
+    , params: ["user.phone":"18307207411","user.password":"123456"
+        ,"deviceType":"android","app_channel":"mytian","client_version":"93"])
 }
 
 let operationQueue = OperationQueue.init()
@@ -135,16 +142,35 @@ class MyUrlSessionsDelegate:NSObject{
 }
 
 
+struct Persons:Decodable,Encodable{
+    var sex:Int8?
+    var age:Int8?
+    var name:String?
+}
+
+var persons = Persons()
+persons.name = "gang.zhou"
+persons.age = 10
+persons.sex = 1
+
+print(Date().timeIntervalSince1970)
+print("Persons : \(String.init(data: (try! JSONEncoder().encode(persons)), encoding: .utf8)!)")
+print(Date().timeIntervalSince1970)
+
 extension MyUrlSessionsDelegate:URLSessionDownloadDelegate{
     func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
         
         do {
             try FileManager.default.moveItem(at: location
                 , to:URL.init(fileURLWithPath: "/Users/gang.zhou/Projects/ios/SwiftDemo/SwiftDemo/download"))
-            print(location.absoluteString)
             try FileManager.default.removeItem(at: location)
-        } catch {
-            
+        } catch _ as NSError{
+            print("处理文件失败")
+            do {
+                try FileManager.default.removeItem(at: location)
+            } catch _ as NSError{
+                print("删除文件失败")
+            }
         }
     }
     
@@ -156,9 +182,112 @@ extension MyUrlSessionsDelegate:URLSessionDownloadDelegate{
 let myUrlSessionDelegate = MyUrlSessionsDelegate()
 
 let myURLSession = URLSession.init(configuration: .default, delegate: myUrlSessionDelegate, delegateQueue: nil)
-myURLSession.downloadTask(with: URL.init(string: "https://www.charlesproxy.com/assets/release/4.2.8/charles-proxy-4.2.8.dmg")!).resume()
+myURLSession.downloadTask(with: URL.init(string: "http://www.baidu.com")!).resume()
+
+
+enum MyError:Error{
+    case Error1
+    case Error2
+    case Error3
+}
+func demoFunc(i : Int) throws ->Int {
+    switch i {
+    case 1:
+        throw MyError.Error1
+    case 2:
+        throw MyError.Error2
+    case 3:
+        throw MyError.Error3
+    default:
+        return i
+    }
+}
 
 
 
-Thread.sleep(forTimeInterval: 1000)
+for i in 1...9 {
+    for j in 1...i {
+        print("\(j) x \(i) = \(i * j)", terminator: "")
+        
+        if (i * j) < 10 {
+            print(" ", terminator: "")
+        }
+        
+        if i != j {
+            print("  ", terminator: "")
+        }
+    }
+    print("")
+}
+
+open class SqliteDatabase:NSObject {
+    private override init() {
+        super.init()
+        self.openDB()
+    }
+    
+    public static let instance:SqliteDatabase = SqliteDatabase()
+    private static let DB_NAME:String = "demo.db"
+    private static let DB_VERSION:Int32 = 2
+    private var db:OpaquePointer? = nil
+    
+    private func openDB(){
+        if SQLITE_OK == sqlite3_open("/Users/gang.zhou/Projects/ios/SwiftDemo/SwiftDemo/\(SqliteDatabase.DB_NAME)".cString(using: .utf8),&db) {
+            print("Database \(SqliteDatabase.DB_NAME):\(SqliteDatabase.DB_VERSION) Open ok!")
+            var ppStmt:OpaquePointer? = nil
+            if SQLITE_OK == sqlite3_prepare_v2(db, "PRAGMA USER_VERSION".cString(using: .utf8)
+                , -1, &ppStmt, nil) {
+                if sqlite3_step(ppStmt) == SQLITE_ROW {
+                    let userVersion = sqlite3_column_int(ppStmt, 0)
+                    print("User Version \(userVersion)")
+                    if 0 == userVersion {
+                        onCreate()
+                    } else if (userVersion < SqliteDatabase.DB_VERSION) {
+                        onUpgrade(oldVersion: userVersion
+                            , newVersion: SqliteDatabase.DB_VERSION)
+                    }
+                    if 0 == userVersion || userVersion < SqliteDatabase.DB_VERSION {
+                        sqlite3_exec(db, "PRAGMA USER_VERSION= \(userVersion)".cString(using: .utf8), nil, nil, nil)
+                    }
+                }
+                sqlite3_finalize(ppStmt)
+            }
+        }
+    }
+    
+    
+    private func onCreate() {
+        print("onCreate")
+    }
+    
+    private func onUpgrade(oldVersion:Int32,newVersion:Int32){
+        print("onUpgrade")
+    }
+}
+
+print(SqliteDatabase.instance)
+print(SqliteDatabase.instance)
+print(SqliteDatabase.instance)
+print(SqliteDatabase.instance)
+
+var db:OpaquePointer? = nil
+if SQLITE_OK ==  sqlite3_open("/Users/gang.zhou/Projects/ios/SwiftDemo/SwiftDemo/demo.db".cString(using: .utf8)!
+    , &db){
+    print("Database open ok!")
+    var err:UnsafeMutablePointer<Int8>? = nil
+    if SQLITE_OK != sqlite3_exec(db, "CREATE TABLE IF NOT EXISTS Persons(_id INTEGER PRIMARY KEY,name TEXT)".cString(using: .utf8)!, nil, nil, &err) {
+        print("exec Fail! Error:\(err!)")
+    } else {
+        for i in 0...1000 {
+            sqlite3_exec(db, "INSERT INTO Persons(name) VALUES ('gang.zhou\(i)')".cString(using: .utf8), nil, nil, nil)
+        }
+    }
+}
+
+
+
+
+
+
+
 
